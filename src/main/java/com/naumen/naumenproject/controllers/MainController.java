46ee64a3da1cb91a6e097418be937164e0c9029c
ayokissme <nikolaev.nikita0209@gmail.com>
2022-03-07 18:05:11 +0500

    first commit

---
+++ src/main/java/com/naumen/naumenproject/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.naumen.naumenproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("name", "Мейн");
+        return "main";
+    }
+
+}
